e67b68d29881835a5cef8c1611718697f5ee2570
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-13 02:15:00 +0530

    Program to demonstrate FOR LOOP in java

---
+++ ForLoop.java
@@ -0,0 +1,20 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate FOR LOOP in java.
+Author  : Chpaone09®
+Date    : Oct 12, 2020
+
+************************************************************
+*/
+
+class ForLoop{
+public static void main(String[] args) {
+    int i =0;
+    System.out.print("\n\n\t Demonstration of FOR LOOP in JAVA >> ");
+    for(i=1;i<=5;i++){
+        System.out.print(" "+i);
+    }
+}
+}
+
